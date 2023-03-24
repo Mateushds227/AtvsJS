8f422ea84f295fbe1a58951ca96af3d091057a39
Mateus Henrique <99653794+Mateushds227@users.noreply.github.com>
2023-03-23 21:05:21 -0300

    Add files via upload

---
+++ atv_memes/script.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', fetchdados())
+var dados = null
+async function fetchdados() {
+    const resposta = await fetch('https://api.imgflip.com/get_memes')
+    dados = await resposta.json()
+    dados = dados.data.memes
+    console.log(dados)
+    memes_total(dados)
+}
+
+var memes = document.querySelector("#memes")
+id = null
+nome = []
+url = null
+
+async function memes_total(dados) {
+    for (dados of dados) {
+        id = dados.id
+        nome = dados.name
+        url = dados.url
+        legenda = null
+        memes.innerHTML += `
+        <div id="${id}" class = "memes" >
+            <div>
+                id  : ${id}
+            </div>
+
+            <div>
+            <img src=${url}>
+            </div> 
+
+            <div>
+                <h3> Nome do meme : ${nome}</h3>
+            </div>
+            
+            </div>
+            <div>
+            <input type="text" id="legenda" name="legenda" placeholder="digite a legenda desse meme" size=100%>
+            </div>
+        <hr>
+    `;
+    }
+}
